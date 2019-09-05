5a7aa313fdd79d38404e65897aa4540d7fb8b0f5
Charlie Crocker <cscrocker84@gmail.com>
2019-09-05 15:57:05 -0500

    working on AJAX call and parsing out the data

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -1,21 +1,67 @@
-$(document).ready(function(){
+$(document).ready(function () {
 
-    var displayedButtons = ["Dogs", "Cats", "Sharks", "Birds"];
+    var topics = ["Dogs", "Cats", "Sharks", "Birds"];
 
 
     // create, dispaly, add data name to btns
-    function renderButtons(){ 
+    function renderButtons() {
 
         $("#buttons").empty();
 
-        for (var i = 0; i < buttons.length; i++){
+        for (var i = 0; i < buttons.length; i++) {
 
-            var newButton = $("<button>") 
+            var newButton = $("<button>")
             newButton.attr("class", "btn btn-default");
-            newButton.attr("id", "input")  
-            newButton.attr("data-name", displayedButtons[i]); 
-            newButton.text(displayedButtons[i]); 
-            $("#buttons").append(newButton); 
+            newButton.attr("id", "input")
+            newButton.attr("data-name", topics[i]);
+            newButton.text(topics[i]);
+            $("#buttons").append(newButton);
         }
     }
-            renderButtons(); 
+    renderButtons();
+
+    function renderGif() {
+
+        $("#gifs").empty();
+        var input = $(this).attr("data-name");
+        var limit = 10;
+        var queryURL = "https://api.giphy.com/v1/gifs/search?q=" + input + "&limit=" + limit + "&api_key=JLAlqk75iH4NTa5Gtdb4b2EXDrJ9HCAF";
+
+        $.ajax({
+            url: queryURL,
+            method: "GET"
+        }).done(function (response) {
+
+            console.log(response);
+
+
+            var results = response.data;
+
+            for (var j = 0; j < limit; j++) {
+
+                var displayDiv = $("<div>");
+                displayDiv.addClass("holder");
+
+                var image = $("<img>");
+                var still = results[j].images.original_still.url;
+                var animate = results[j].images.original.urll;
+
+                image.attr("src", results[j].images.original_still.url);
+                image.attr("data-still", results[j].images.original_still.url);
+                image.attr("data-animate", results[j].images.original.url);
+                image.attr("data-state", "still");
+                image.attr("class", "gif");
+                displayDiv.append(image);
+
+
+                var rating = response.data[j].rating;
+                console.log(rating);
+
+                var R = $("<p>").text("Rating:-" + rating);
+                displayDiv.prepend(R)
+
+                $("#display-images").append(displayDiv);
+            }
+            })
+        }
+    
