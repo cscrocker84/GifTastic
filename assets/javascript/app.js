6204b9a8e6d968e70b84ecb01a52d55cc34d179c
Charlie Crocker <cscrocker84@gmail.com>
2019-09-05 15:04:47 -0500

    Getting functionality of buttons in order

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -1,3 +1,21 @@
-let topics = ['Cats', 'Dogs', 'Birds', 'Tigers', 'Lions'];
-let limit = 10;
-var toSearch = '';
+$(document).ready(function(){
+
+    var displayedButtons = ["Dogs", "Cats", "Sharks", "Birds"];
+
+
+    // create, dispaly, add data name to btns
+    function renderButtons(){ 
+
+        $("#buttons").empty();
+
+        for (var i = 0; i < buttons.length; i++){
+
+            var newButton = $("<button>") 
+            newButton.attr("class", "btn btn-default");
+            newButton.attr("id", "input")  
+            newButton.attr("data-name", displayedButtons[i]); 
+            newButton.text(displayedButtons[i]); 
+            $("#buttons").append(newButton); 
+        }
+    }
+            renderButtons(); 
